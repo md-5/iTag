aef591f64d70c183b84b40db75169fa336451345
md_5 <git@md-5.net>
2014-02-01 17:18:30 +1100

    Add TagAPIException class for full compatability with TagAPI. Closes #2

---
+++ src/main/java/org/kitteh/tag/api/TagAPIException.java
@@ -0,0 +1,17 @@
+package org.kitteh.tag.api;
+
+public class TagAPIException extends RuntimeException
+{
+
+    private static final long serialVersionUID = 1L;
+
+    public TagAPIException(String message)
+    {
+        super( message );
+    }
+
+    public TagAPIException(String message, Throwable throwable)
+    {
+        super( message, throwable );
+    }
+}
